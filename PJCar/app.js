e56500cfe2ce3080df83e3698dae6efd89ef585a
hieunguyengcs <nguyenvanhieubtxqn@gmail.com>
2018-10-27 12:48:56 +0700

    Implement: Update for test upload/download image file from aws S3 bucket

--- PJCar/app.js
+++ PJCar/app.js
@@ -3,12 +3,19 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var fs = require("fs");
+var busboy = require('connect-busboy');
+var busboyBodyParser = require('busboy-body-parser');
+const Busboy = require('busboy');
 
 //gan thu muc tinh de doc duoc trong ejs
 app.use(express.static(__dirname + '/css'));
 app.use(express.static(__dirname + '/js'));
 app.use(express.static(__dirname + '/img'));
 app.use(express.static(__dirname + '/ckeditor'));
+app.use(busboy());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(busboyBodyParser());
 aws.config.update({
     region: "us-west-2",
     endpoint: "http://localhost:8000"
@@ -193,4 +200,129 @@ app.post("/testupload", urlbodyParser, function (req, res) {
     console.log(req.body);
     console.log(req.header);
     console.log(req.query);
+})
+
+// Prepare for upload
+const BUCKET_NAME = 'test-bucket-hieunguyen';
+const IAM_USER_KEY = 'AKIAIKHCXLMLZ4G3BTDA';
+const IAM_USER_SECRET = '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';
+function uploadToS3(file) {
+    aws.config.update({
+        region: "us-west-2",
+        endpoint: "s3.us-west-2.amazonaws.com"
+    });
+
+    let s3bucket = new aws.S3({
+        accessKeyId: IAM_USER_KEY,
+        secretAccessKey: IAM_USER_SECRET,
+        Bucket: BUCKET_NAME,
+    });
+    s3bucket.createBucket(function () {
+        var params = {
+            Bucket: BUCKET_NAME,
+            Key: file.name,
+            Body: file.data,
+        };
+        s3bucket.upload(params, function (err, data) {
+            if (err) {
+                console.log('error in callback');
+                console.log(err);
+            }
+            console.log('success');
+            console.log(data);
+        });
+    });
+}
+
+// Get image method
+function getFromS3(name, callback) {
+    aws.config.update({
+        region: "us-west-2",
+        endpoint: "s3.us-west-2.amazonaws.com"
+    });
+
+    let s3bucket = new aws.S3({
+        accessKeyId: IAM_USER_KEY,
+        secretAccessKey: IAM_USER_SECRET,
+        Bucket: BUCKET_NAME,
+    });
+
+    var params = {
+        Bucket: BUCKET_NAME,
+        Key: name
+    };
+    s3bucket.getObject(params, function (err, data) {
+        if (err) {
+            console.log(err, err.stack);
+        } // an error occurred
+        else {
+            console.log(data);
+        }       // successful response
+        callback(data, err);
+
+        /*
+        data = {
+         AcceptRanges: "bytes", 
+         ContentLength: 3191, 
+         ContentType: "image/jpeg", 
+         ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"", 
+         LastModified: <Date Representation>, 
+         Metadata: {
+         }, 
+         TagCount: 2, 
+         VersionId: "null"
+        }
+        */
+    });
+}
+
+// Upload to aws S3
+app.post('/api/upload', function (req, res, next) {
+    // This grabs the additional parameters so in this case passing     
+    // in "element1" with a value.
+    const element1 = req.body.element1;
+    var busboy = new Busboy({ headers: req.headers });
+    // The file upload has completed
+    busboy.on('finish', function () {
+        console.log('Upload finished');
+        // Your files are stored in req.files. In this case,
+        // you only have one and it's req.files.element2:
+        // This returns:
+        // {
+        //    element2: {
+        //      data: ...contents of the file...,
+        //      name: 'Example.jpg',
+        //      encoding: '7bit',
+        //      mimetype: 'image/png',
+        //      truncated: false,
+        //      size: 959480
+        //    }
+        // }
+        // Grabs your file object from the request.
+        const file = req.files.element2;
+        console.log(file);
+        uploadToS3(file);
+    });
+    req.pipe(busboy);
+
+    res.end("Upload success.");
+});
+
+app.get("/getimage", urlbodyParser, function (req, res) {
+    res.render('test-get-image');
+})
+
+// Test get img from S3
+app.get("/api/getimage", urlbodyParser, function (req, res) {
+
+    console.log(req.query.imgname);
+    name = req.query.imgname;
+    getFromS3(name, function (data, err) {
+        if (err) {
+            res.send("Fail to get image file.");
+        } else {
+            res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+            res.end(data.Body);
+        }
+    })
 })
---
+++ PJCar/server/routes/api/file.js
@@ -0,0 +1,11 @@
+
+module.exports = (app) => {
+    // The following is an example of making file upload with 
+    // additional body parameters.
+    // To make a call with PostMan
+    // Don't put any headers (content-type)
+    // Under body:
+    // check form-data
+    // Put the body with "element1": "test", "element2": image file
+    
+}
